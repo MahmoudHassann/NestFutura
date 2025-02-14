faae3ed93477857f3beec9160b9678cd76f71c7b
Mahmoud Mohamed <68821734+MahmoudHassann@users.noreply.github.com>
2025-02-14 16:51:51 +0200

    server ssr

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,14 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-/*   {
+  {
     path: 'products/:id',
     renderMode: RenderMode.Server // ✅ Ensures SSR for product details pages
   },
   {
     path:'page/:category',
     renderMode:RenderMode.Server
-  }, */
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
